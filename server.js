92c808cd1b1c53af09b0ba3a665ff9217db05197
PizzieT <167532437+PizzieT@users.noreply.github.com>
2025-06-09 05:29:40 -0600

    Update server.js

--- server.js
+++ server.js
@@ -51,5 +51,5 @@ app.get('/stores', async (req, res) => {
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(🚀 Server is running on port ${PORT});
+ console.log(`🚀 Server is running on port ${PORT}`);
 });
